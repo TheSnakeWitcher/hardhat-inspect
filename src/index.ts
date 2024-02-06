ff1d5c01d6fbadddbd3ce331a33e9c2a3e845791
Mr.PAPI <thesnakewitcher@gmail.com>
2024-02-06 15:00:51 -0500

    feat: add inspect object to hre and data to hre.config.paths

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { extendConfig, extendEnvironment } from "hardhat/config";
+import { lazyObject } from "hardhat/plugins";
+import { HardhatConfig, HardhatUserConfig } from "hardhat/types";
+import path from "path";
+
+import { Inspect } from "./inspect";
+import "./type-extensions";
+
+extendConfig(
+  (config: HardhatConfig, userConfig: Readonly<HardhatUserConfig>) => {
+    const userPath = userConfig.paths?.data;
+
+    let dataPath: string;
+    if (userPath === undefined) {
+      dataPath = path.join(config.paths.root, "data");
+    } else {
+      if (path.isAbsolute(userPath)) {
+        dataPath = userPath;
+      } else {
+        dataPath = path.normalize(path.join(config.paths.root, userPath));
+      }
+    }
+
+    config.paths.data = dataPath;
+  }
+);
+
+extendEnvironment((hre: any) => {
+  hre.inspect = lazyObject(() => new Inspect() );
+});
---
+++ src/type-extensions.ts
@@ -0,0 +1,20 @@
+import "hardhat/types/config";
+import "hardhat/types/runtime";
+
+import { Inspect } from "./inspect";
+
+declare module "hardhat/types/config" {
+  export interface ProjectPathsUserConfig {
+    data?: string;
+  }
+
+  export interface ProjectPathsConfig {
+    data: string;
+  }
+}
+
+declare module "hardhat/types/runtime" {
+  export interface HardhatRuntimeEnvironment {
+    inspect: Inspect ;
+  }
+}
