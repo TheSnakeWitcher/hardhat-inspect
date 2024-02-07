15ed38c7c6ab47a96fa2b55845a8ebd77556f2b5
Mr.PAPI <thesnakewitcher@gmail.com>
2024-02-06 22:00:41 -0500

    feat: overwrite compile task to run inspected.save() after compile

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { extendConfig, extendEnvironment } from "hardhat/config";
+import { extendConfig, extendEnvironment, task } from "hardhat/config";
 import { lazyObject } from "hardhat/plugins";
 import { HardhatConfig, HardhatUserConfig } from "hardhat/types";
 import path from "path";
@@ -6,7 +6,7 @@ import path from "path";
 import { Inspect } from "./inspect";
 import "./type-extensions";
 
-extendConfig((config: HardhatConfig, userConfig: Readonly<HardhatUserConfig>) => {
+extendConfig( (config: HardhatConfig, userConfig: Readonly<HardhatUserConfig>) => {
     const userPath = userConfig.paths?.data;
 
     let dataPath: string;
@@ -21,9 +21,15 @@ extendConfig((config: HardhatConfig, userConfig: Readonly<HardhatUserConfig>) =>
     }
 
     config.paths.data = dataPath;
-}
-);
+});
 
-extendEnvironment((hre: any) => {
+extendEnvironment( (hre: any) => {
     hre.inspect = lazyObject(() => new Inspect());
 });
+
+task("compile", async function(args: any, hre: any, runSuper: any) {
+    await runSuper(args);
+    const inspect = new Inspect()
+    const { contractNames, events, errors } = await inspect.refresh()
+    await inspect.save(contractNames, events, errors)
+})
